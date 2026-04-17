baa0e9515dec07814d4970117c3dd6e1bdca6525
Mostafa Khaled <200045191@must.edu.eg>
2026-04-17 18:29:48 +0200

    Your commit message here

--- middlewares/csrf-token.js
+++
@@ -1,7 +0,0 @@
-
-function addCsrfToken(req, res, next) {
-  res.locals.csrfToken = req.csrfToken();
-  next();
-}
-
-module.exports = addCsrfToken;
--- public/scripts/mobile.js
+++
@@ -1,9 +0,0 @@
-const mobileMenuBtnElement = document.getElementById('mobile-menu-btn')
-const mobileMenuElement =document.getElementById('mobile-menu')
-
-function toggleMobileMenu(){
-
-    mobileMenuElement.classList.toggle('open')
-
-} 
-mobileMenuBtnElement.addEventListener('click',toggleMobileMenu)
