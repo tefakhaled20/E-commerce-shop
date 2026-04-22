f06b936930dc953169ce23b357736427a9997b45
mostafa khaled <200045191@must.edu.eg>
2026-04-22 14:53:14 +0200

    Switch multer storage to memory
    Changed multer storage from disk to memory for image uploads.

--- middlewares/image-upload.js
+++ middlewares/image-upload.js
@@ -3,12 +3,7 @@ const multer =require('multer')
 const uuid = require('uuid').v4;
 
 const upload = multer({
-  storage: multer.diskStorage({
-    destination: 'product-data/images',
-    filename: function(req, file, cb) {
-      cb(null, uuid() + '-' + file.originalname);
-    }
-  })
+    storage: multer.memoryStorage(),
 });
 
 const configuredMulterMiddleware = upload.single('image');
